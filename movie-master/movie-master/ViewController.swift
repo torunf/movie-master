d66bb2763d4f662f46ab77d0b94de0a2fc6bd13c
Furkan torun <ftorun@gmail.com>
2021-11-22 22:41:34 +0300

    App created first time

---
+++ movie-master/movie-master/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  movie-master
+//
+//  Created by Furkan Torun on 22.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
